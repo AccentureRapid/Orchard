64489be50a7734db256b9f030f43c3b31876b7cd
david <david.bingjian.yu>
2017-08-31 20:22:27 +0800

    setup the module

---
+++ src/Orchard.Web/Modules/Orchard.SuperRocket/Common/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Orchard.SuperRocket.AdSystem.Common
+{
+    public static class Utilities
+    {
+        public static StringContent Serialize(dynamic source, HttpResponseMessage response)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                ContractResolver = new NullToEmptyStringResolver(),
+                PreserveReferencesHandling = PreserveReferencesHandling.None
+            };
+
+            var stringcontent = JsonConvert.SerializeObject(source, Newtonsoft.Json.Formatting.Indented, settings);
+            return new StringContent(stringcontent, Encoding.GetEncoding("UTF-8"), "application/json");
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Orchard.SuperRocket/Extension/HttpRoutes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Orchard.Mvc.Routes;
+using Orchard.WebApi.Routes;
+using System.Web.Http;
+
+namespace Orchard.SuperRocket.AdSystem.Extension
+{
+    public class HttpRoutes : IHttpRouteProvider
+    {
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (RouteDescriptor routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+            new HttpRouteDescriptor {
+                Name = "Orchard.SuperRocket.AdSystem.Api",
+                Priority = -10,
+                RouteTemplate = "api/{controller}/{action}/{id}",
+                Defaults = new {
+                    area = "Orchard.SuperRocket.AdSystem",
+                    id = RouteParameter.Optional
+                },
+            }
+        };
+        }
+    }
+}
